ccfa804e8ae1f975cec189fe5833dbeb2fd34533
Sandli Singh <sandli1812singh@gmail.com>
2026-08-17 23:47:16 +0530

    Added SQL basics

---
+++ 01-basics.sql
@@ -0,0 +1,41 @@
+</>SQL 
+
+--SQL BASICS
+--My SQL Learning Journey
+
+--1.Data
+Raw facts and figures.
+
+--2.information
+-- Processed and meaningful data.
+
+--3.SQL
+SQL(structured Query Language) is a standard language used to communicate with relational databases and perform operations such as retrieving, inserting,updating and deleting data.
+
+--4.Database
+--organized collection of related data.
+
+--5.DBMS
+--Software used to create,store, manage and retrieve data.
+
+--6.Relational Model 
+--DBMS that stores data in tables using rows and colmns.
+
+--7.Row
+--A singlr record in a table.
+
+--8.Column
+--An attribute/field of a table.
+
+--9.Cell
+-- intersection of row and a column.
+
+--10.SQL Commands 
+--a sql commands is an instruction written in sql to perform a specific operation on a database
+-- sql commands
+ a.DDL
+ b.DML 
+ c.DQL
+ d.DCL
+ f.TCL
+ 
